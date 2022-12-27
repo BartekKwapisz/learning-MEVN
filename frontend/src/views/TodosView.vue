38a9016f8ca690c549435b19c72cc79789c1b914
Bartek Kwapisz <kwapiszbartek@gmail.com>
2022-12-27 05:36:57 +0100

    frontend folder added
    https://www.youtube.com/watch?v=AHtlHRTyfKk&list=PL2jXTHYir9I4iRHND0H3nOA1EwxZOWCAz&index=14

---
+++ frontend/src/views/TodosView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div v-for="item in state.todos" :key="item.author">
+            <h4>{{ item.author }}</h4>
+            <p>{{ item.todo }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { reactive, onMounted } from 'vue';
+export default {
+    setup() {
+        const state = reactive({
+            todos: {
+
+            }
+        });
+        function GetAll() {
+            fetch("http://localhost:3000/todos")
+                .then(res => res.json())
+                .then(data => {
+                    state.todos = data
+ 
+                })
+        } 
+        onMounted(() => {
+            GetAll()
+        });
+        return { state, GetAll }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
